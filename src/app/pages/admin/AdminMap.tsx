cb341e799b5b9d8e05c030f8391d2234a00feecc
zakariyasktc <zytechinsightforge@gmail.com>
2026-09-15 02:59:48 +0100

    Fix maplibre import

--- src/app/pages/admin/AdminMap.tsx
+++ src/app/pages/admin/AdminMap.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import maplibregl from "maplibre-gl";
+import * as maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 const LAGOS_CENTER: [number, number] = [3.3792, 6.5244];
